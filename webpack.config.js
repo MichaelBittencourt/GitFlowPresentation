b346e2b511193db6ef9ff66287c6d983ebb1b826
Michael Bittencourt <mchl.bittencourt@gmail.com>
2022-08-26 19:31:34 -0300

    feature: Create template

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,34 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ttf|woff|eot)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
-      },
-      {
-        test: /\.css$/,
-        use:['style-loader','css-loader']
-      }
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({template: 'index.html'})
-  ]
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ttf|woff|eot)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: 'index.html' }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'images', to: 'images' },
+                { from: 'videos', to: 'videos' },
+            ],
+        }),
+    ]
 };
